bbf7544361bf69c6c90a03e59f39ef7fe839a7d2
Melissa Nicolich <melissa.nicolich@gmail.com>
2018-10-17 00:48:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 let currentUser = 'melissa'
 let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
 let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!'
-let shortGreeting = 'Welcome, ' + 
+let shortGreeting = 'Welcome, ' + currentUser.charAt(0)
